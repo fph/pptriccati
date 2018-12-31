de8d25615f538fc9667526917b6b9167e370a397
Federico Poloni <fpoloni@di.unipi.it>
2018-12-31 16:37:37 +0100

    added an utility function to work with various representations

---
+++ symFromQuad.m
@@ -0,0 +1,14 @@
+function sym = symFromQuad(quad)
+% creates a 'symBasis', i.e., an explicit PPT, from a factored quadBasis
+
+sym.p = quad.p;
+sym.dimensions = quad.dimensions;
+
+n1 = quad.dimensions(1);
+n2 = quad.dimensions(2);
+
+A = quad.X(1:n1, end-n2+1:end);
+B = quad.X(1:n1, 1:end-n2);
+C = quad.X(n1+1:end, end-n2+1:end);
+
+sym.X = [-B*B' A; A' C'*C];
